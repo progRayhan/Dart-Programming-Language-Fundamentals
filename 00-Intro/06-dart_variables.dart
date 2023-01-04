19c2404d5167b98ae2721f474fe0120689180ebf
Rayhan <prog.rayhan1@gmail.com>
2023-01-04 16:05:34 +0600

    dart variable added

---
+++ 00-Intro/06-dart_variables.dart
@@ -0,0 +1,23 @@
+void main(){
+  // Dart Variable
+  var name = "Rayhan";
+  
+  int age = 10;
+
+  String msg = "Welcome to Your-Campus";
+
+  // Declaring the variable with Multiple Values
+  int i,j,k;
+  i = 5;
+  j = 10;
+  k = 15;
+
+  // final variable without type annotation
+  final name2 = 'Ricky'; // Final variable can't be changed
+
+  // final variable with type annotation
+  final String msg2 = 'How are you?';
+
+  // constant variable
+  const a = 1000;
+}
